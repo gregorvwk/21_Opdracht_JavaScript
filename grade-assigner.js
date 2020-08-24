36bdd97239fc1cf2a63056829ca16d8f1f5e7352
gregorvwk <69797933+gregorvwk@users.noreply.github.com>
2020-08-24 11:27:22 +0200

    Add files via upload

---
+++ grade-assigner.js
@@ -0,0 +1,18 @@
+
+
+for (score = 60; score <= 100; score++){
+    function assignGrade(score) {
+        if (score > 90) {
+            return 'A';
+        } else if (score > 80) {
+            return 'B';
+        } else if (score > 70) {
+            return 'C';
+        } else if (score > 65) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+    console.log("voor " + score + " punten, krijg je een " + assignGrade(score));
+}
---
+++ multiplication-tables.js
@@ -0,0 +1,5 @@
+for (x = 1; x <= 10; x++){
+    for (i = 1; i <= 10; i++){
+        console.log(i + " X " + x + " = " + i * x);
+    }
+}
